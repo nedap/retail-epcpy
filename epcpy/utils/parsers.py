53120d934603cedd9c64d4375fc0e0f2c60dafbd
Sander Meinderts <sander.meinderts@nedap.com>
2022-02-16 09:55:50 +0100

    Update parsers to include nice check for EPC scheme and complete lists of schemes for GS1 keyed

--- epcpy/utils/parsers.py
+++ epcpy/utils/parsers.py
@@ -8,22 +8,61 @@ from epcpy.epc_schemes.cpi import CPI
 from epcpy.epc_schemes.gdti import GDTI
 from epcpy.epc_schemes.giai import GIAI
 from epcpy.epc_schemes.gid import GID
+from epcpy.epc_schemes.ginc import GINC
 from epcpy.epc_schemes.grai import GRAI
+from epcpy.epc_schemes.gsin import GSIN
 from epcpy.epc_schemes.gsrn import GSRN
 from epcpy.epc_schemes.gsrnp import GSRNP
+from epcpy.epc_schemes.imovn import IMOVN
 from epcpy.epc_schemes.itip import ITIP
+from epcpy.epc_schemes.pgln import PGLN
 from epcpy.epc_schemes.sgcn import SGCN
 from epcpy.epc_schemes.sgln import SGLN
 from epcpy.epc_schemes.sgtin import SGTIN
 from epcpy.epc_schemes.sscc import SSCC
+from epcpy.epc_schemes.upui import UPUI
 from epcpy.epc_schemes.usdod import USDOD
 from epcpy.utils.common import ConvertException, base64_to_hex, hex_to_binary
 from epcpy.utils.regex import TAG_URI
 
 TAG_URI_REGEX = re.compile(TAG_URI)
 
-EPC_SCHEMES: List[EPCScheme] = [ADI, BIC, CPI, SGTIN]  # TODO
-GS1_KEYED_CLASSES: List[GS1Keyed] = [SGTIN]  # TODO
+EPC_SCHEMES: List[EPCScheme] = [
+    ADI,
+    BIC,
+    CPI,
+    GDTI,
+    GIAI,
+    GID,
+    GINC,
+    GRAI,
+    GSIN,
+    GSRN,
+    GSRNP,
+    IMOVN,
+    ITIP,
+    PGLN,
+    SGCN,
+    SGLN,
+    SGTIN,
+    SSCC,
+    UPUI,
+    USDOD,
+]
+GS1_KEYED_CLASSES: List[GS1Keyed] = [
+    GDTI,
+    GIAI,
+    GINC,
+    GRAI,
+    GSIN,
+    GSRN,
+    GSRNP,
+    PGLN,
+    SGCN,
+    SGLN,
+    SGTIN,
+    SSCC,
+]
 TAG_ENCODABLE_CLASSES: List[TagEncodable] = [
     ADI,
     CPI,
@@ -45,22 +84,19 @@ TAG_ENCODABLE_HEADERS = {
     h.value: cls for cls in TAG_ENCODABLE_CLASSES for h in cls.BinaryHeader
 }
 
+EPC_SCHEME_IDENTIFIERS = {cls.__name__.lower(): cls for cls in EPC_SCHEMES}
+
 
 def epc_pure_identity_to_scheme(epc_pure_identity_uri: str) -> EPCScheme:
+    identifier = epc_pure_identity_uri.split(":")[3]
 
-    for cls in EPC_SCHEMES:
-        try:
-            return cls.from_epc_uri(epc_pure_identity_uri)
-        except:
-            continue
+    if identifier not in EPC_SCHEME_IDENTIFIERS:
+        raise ConvertException("Unknown EPC URI")
 
-    raise ConvertException(
-        "Unable to find suitable EPCScheme class for given uri string"
-    )
+    return EPC_SCHEME_IDENTIFIERS[identifier].from_epc_uri(epc_pure_identity_uri)
 
 
 def epc_pure_identity_to_gs1_keyed(epc_pure_identity_uri: str) -> GS1Keyed:
-
     scheme = epc_pure_identity_to_scheme(epc_pure_identity_uri)
 
     if not isinstance(scheme, GS1Keyed):
